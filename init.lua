acbf5a1a841cb0b2098771b6b824a10ebc33d55f
roman.bondarenko <romaha2901@gmail.com>
2023-10-23 19:58:32 +0300

    Autocommands
    Add dotenv, vevn select commands on enter project
    Some generic configurations

--- init.lua
+++ init.lua
@@ -1,8 +1,24 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
---
+
+autocmd("VimEnter", {
+  command = "silent! :Dotenv",
+})
+autocmd("BufReadPre", {
+  pattern = "*.py",
+  command = ":VenvSelectCached",
+  once = true,
+})
+
+-- Set relative number
+vim.wo.relativenumber = true
+vim.opt.relativenumber = true
+
+-- Wrap lines
+vim.opt.wrap = false
+vim.opt.spell = true
